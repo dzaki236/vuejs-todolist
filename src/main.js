1f08df9350a8c3652eb6f8bc00390b9d8d2fd4e6
dzaki236 <dzakiahnafz@gmail.com>
2021-09-18 21:25:14 +0700

    add somethink check this out!

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
+import "bootstrap/dist/css/bootstrap.min.css";
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    router,
+    store,
+    render: (h) => h(App),
 }).$mount("#app");
